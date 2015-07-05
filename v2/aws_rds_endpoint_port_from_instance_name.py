c618eb17c50f8b7a42ac335e978cdd67d6c86c1d
Jon Hadfield <jon@lessknown.co.uk>
2015-07-05 16:52:07 +0100

    add rds endpoint lookups.

---
+++ v2/aws_rds_endpoint_port_from_instance_name.py
@@ -0,0 +1,29 @@
+# (c) 2015, Jon Hadfield <jon@lessknown.co.uk>
+"""
+Description: This lookup takes an AWS region and an RDS instance
+name and returns the endpoint port.
+
+Example Usage:
+{{ lookup('aws_rds_endpoint_port_from_instance_name', ('eu-west-1', 'mydb')) }}
+"""
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import *
+from ansible.plugins.lookup import LookupBase
+
+try:
+    import boto.rds
+except ImportError:
+    raise AnsibleError("aws_rds_endpoint_port_from_instance_name lookup cannot be run without boto installed")
+
+
+class LookupModule(LookupBase):
+    def run(self, terms, variables=None, **kwargs):
+        region = terms[0]
+        instance_name = terms[1]
+        db_conn = boto.rds.connect_to_region(region)
+        db = db_conn.get_all_dbinstances(instance_name)
+        if db and db[0]:
+            return [str(db[0].endpoint[1])]
+        return None
